6e479751408767186eafb5be4ba7dbb88686fcd8
jfedgar <jfedgar@gmail.com>
2016-07-23 09:11:12 -0400

    Add types

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723054333) do
+ActiveRecord::Schema.define(version: 20160723091956) do
 
   create_table "amenities", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -27,6 +27,24 @@ ActiveRecord::Schema.define(version: 20160723054333) do
 
   add_index "amenities_properties", ["amenity_id"], name: "index_amenities_properties_on_amenity_id", using: :btree
   add_index "amenities_properties", ["property_id"], name: "index_amenities_properties_on_property_id", using: :btree
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "full_name",  limit: 255
+    t.string   "facet_name", limit: 255
+    t.string   "data_name",  limit: 255
+    t.float    "latitude",   limit: 24
+    t.float    "longitude",  limit: 24
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "locations_properties", id: false, force: :cascade do |t|
+    t.integer "location_id", limit: 4
+    t.integer "property_id", limit: 4
+  end
+
+  add_index "locations_properties", ["location_id"], name: "index_locations_properties_on_location_id", using: :btree
+  add_index "locations_properties", ["property_id"], name: "index_locations_properties_on_property_id", using: :btree
 
   create_table "properties", force: :cascade do |t|
     t.string   "address1",       limit: 255
@@ -46,6 +64,21 @@ ActiveRecord::Schema.define(version: 20160723054333) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.date     "rented_at"
+    t.integer  "location_id",    limit: 4
+  end
+
+  create_table "properties_types", id: false, force: :cascade do |t|
+    t.integer "type_id",     limit: 4
+    t.integer "property_id", limit: 4
+  end
+
+  add_index "properties_types", ["property_id"], name: "index_properties_types_on_property_id", using: :btree
+  add_index "properties_types", ["type_id"], name: "index_properties_types_on_type_id", using: :btree
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
