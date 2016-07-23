9075f5187c46173c29910656bc122c32a7112054
jfedgar <jfedgar@gmail.com>
2016-07-23 01:11:17 -0400

    start facet query, index associations

--- app/models/property.rb
+++ app/models/property.rb
@@ -15,5 +15,33 @@ class Property < ActiveRecord::Base
     address1 + " " + address2.to_s + " " + zipcode
   end
 
+  def get_fresh_facets(facets)
+    {
+      size: 0,
+      query: {
+        filtered: {
+          query: { match_all: {} },
+          filter: {
+            and: and_array(facets)
+          }
+        }
+      } 
+      aggs: {
+      }
+    }
+  end
+
+  def and_array(facets)
+    []
+  end
+
+  def as_indexed_json(options={})
+    self.as_json(
+      include: {
+        amenities: { only: :name },
+      }
+    )
+  end
+
 
 end
