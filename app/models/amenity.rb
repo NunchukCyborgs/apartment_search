fd9229128cb0eaba8bfa625845877d764807a13e
jfedgar <jfedgar@gmail.com>
2016-07-23 09:11:41 -0400

    Alter amenities

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,3 +1,8 @@
 class Amenity < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   validates :name, presence: true
+
+  has_and_belongs_to_many :properties
 end
--- db/migrate/20160723054333_create_amenities.rb
+++ db/migrate/20160723054333_create_amenities.rb
@@ -2,7 +2,6 @@ class CreateAmenities < ActiveRecord::Migration
   def change
     create_table :amenities do |t|
       t.string :name
-      t.string :timestamps
 
       t.timestamps null: false
     end
