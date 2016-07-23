255e1a3ff61c0a18f62b2be4ad8a474a8aa0a475
jfedgar <jfedgar@gmail.com>
2016-07-22 22:54:56 -0400

    Add geocoding

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'foundation-rails'
 gem 'elasticsearch-model'
 gem 'elasticsearch-rails'
+gem 'geocoder'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,6 +3,13 @@ class Property < ActiveRecord::Base
 
   #for contact_number contact_email, maybe we default back to these properties
   #on the owner if they have not entered them for the property 
+  
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    address1 + " " + address2.to_s + " " + zipcode
+  end
 
 
 end
