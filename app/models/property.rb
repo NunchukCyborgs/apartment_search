bc3971705fb4be669d0091647824a3d6bcd3e148
jfedgar <jfedgar@gmail.com>
2016-07-22 23:20:33 -0400

    add elasticsearch support

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,15 @@
 class Property < ActiveRecord::Base
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  geocoded_by :full_address
+  after_validation :geocode
+
   validates :address1, :zipcode, :price, :lease_length, presence: true
 
   #for contact_number contact_email, maybe we default back to these properties
   #on the owner if they have not entered them for the property 
-  
-  geocoded_by :full_address
-  after_validation :geocode
 
   def full_address
     address1 + " " + address2.to_s + " " + zipcode
