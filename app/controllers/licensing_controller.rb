4ed70654d9e4833c7fa148a52a58ccd474c7d5b7
Stephen Myers <scmyers11@gmail.com>
2016-09-19 12:31:32 -0500

    Changes status code for user already having license

--- app/controllers/licensing_controller.rb
+++ app/controllers/licensing_controller.rb
@@ -3,7 +3,8 @@ class LicensingController < ApplicationController
   def authenticate
     status_404 and return unless params[:license_id]
     license = License.find_by(value: params[:license_id])
-    status_404 and return if license.nil? || license.claimed? || current_user.has_license?
+    status_404 and return if license.nil? || license.claimed?
+    status_unprocessable and return if current_user.has_license?
     current_user.process_license license
   end
 
@@ -11,6 +12,10 @@ class LicensingController < ApplicationController
 
   def status_404
     render json: { result: "failed" }, status: 404
+  end
+
+  def status_unprocessable
+    render json: { result: "failed" }, status: :unprocessable_entity
   end
 
 end
