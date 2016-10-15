dc5db59411d7dc520d41eafd69d41a344e73e1e8
Stephen Myers <scmyers11@gmail.com>
2016-10-15 11:52:53 -0500

    Fix stephen not being able to type

--- app/models/property.rb
+++ app/models/property.rb
@@ -47,7 +47,7 @@ class Property < ActiveRecord::Base
 
   delegate :primary_contact, to: :license, prefix: true, allow_nil: true
 
-  scope :like_address, ->(address_string) { where("address1 LIKE ?", "%#{address_string}%")
+  scope :like_address, ->(address_string) { where("address1 LIKE ?", "%#{address_string}%") }
 
   after_create :set_locations
   geocoded_by :full_address
