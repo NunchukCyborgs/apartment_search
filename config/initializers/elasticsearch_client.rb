c3b5e5d801276fbdef4292177d59be3fe73412af
jfedgar <jfedgar@gmail.com>
2016-10-31 15:38:34 -0500

    set port es

--- config/initializers/elasticsearch_client.rb
+++ config/initializers/elasticsearch_client.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Elasticsearch::Model.client = Elasticsearch::Client.new host: 'search-es-prod-pxdox4tjiyhrua46eogkc7lvra.us-west-2.es.amazonaws.com'
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: 'search-es-prod-pxdox4tjiyhrua46eogkc7lvra.us-west-2.es.amazonaws.com:80'
 else
   Elasticsearch::Model.client = Elasticsearch::Client.new log: true
 end
