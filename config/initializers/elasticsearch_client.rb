4c1e89b662abe87ba70359ad624c35654f8f7317
jfedgar <jfedgar@gmail.com>
2017-01-09 02:13:09 -0600

    Move elasticsearch

--- config/initializers/elasticsearch_client.rb
+++ config/initializers/elasticsearch_client.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Elasticsearch::Model.client = Elasticsearch::Client.new host: 'search-es-prod-pxdox4tjiyhrua46eogkc7lvra.us-west-2.es.amazonaws.com:80'
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: '10.128.0.4'
 else
   Elasticsearch::Model.client = Elasticsearch::Client.new log: true
 end
